5b67c3f2fc2beb9261f7a2df067ef4f83dae2401
yychuiyan <yychuiyan@outlook.com>
2023-08-30 18:04:30 +0800

    feat:路由配置

---
+++ zhihu-app/src/router/index.jsx
@@ -0,0 +1,38 @@
+import React, { Suspense } from 'react';
+import {
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+  useParams,
+  useSearchParams,
+} from 'react-router-dom';
+import routes from './routes';
+
+// 统一渲染路由配置
+
+const Element = function Element(props) {
+  let { component: Component, meta } = props;
+  // 修改页面title
+  let { title = '知乎日报' } = meta || {};
+  document.title = title;
+  // 获取路由信息，基于属性传递给组件
+  const navigate = useNavigate(),
+    location = useLocation(),
+    params = useParams(),
+    [usp] = useSearchParams();
+  return <Component navigate={navigate} location={location} params={params} usp={usp} />;
+};
+
+export default function RouterView() {
+  return (
+    <Suspense fallback={<></>}>
+      <Routes>
+        {routes.map(item => {
+          let { name, path } = item;
+          return <Route key={name} path={path} element={<Element {...item} />} />;
+        })}
+      </Routes>
+    </Suspense>
+  );
+}
---
+++ zhihu-app/src/router/routes.jsx
@@ -0,0 +1,71 @@
+import { lazy } from 'react';
+// 默认展示首页 打包到js中
+import Home from '../views/Home';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    // 浏览器标签显示内容
+    meta: {
+      title: '首页-知乎日报',
+    },
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    component: lazy(() => import('../views/Detail')),
+    // 浏览器标签显示内容
+    meta: {
+      title: '新闻详情-知乎日报',
+    },
+  },
+  {
+    path: '/personal',
+    name: 'personal',
+    component: lazy(() => import('../views/Personal')),
+    // 浏览器标签显示内容
+    meta: {
+      title: '个人中心-知乎日报',
+    },
+  },
+  {
+    path: '/store',
+    name: 'store',
+    component: lazy(() => import('../views/Store')),
+    // 浏览器标签显示内容
+    meta: {
+      title: '我的收藏-知乎日报',
+    },
+  },
+  {
+    path: '/update',
+    name: 'update',
+    component: lazy(() => import('../views/Update')),
+    // 浏览器标签显示内容
+    meta: {
+      title: '修改个人信息-知乎日报',
+    },
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: lazy(() => import('../views/Login')),
+    // 浏览器标签显示内容
+    meta: {
+      title: '登录注册-知乎日报',
+    },
+  },
+  {
+    path: '*',
+    name: '404',
+    component: lazy(() => import('../views/Page404')),
+    // 浏览器标签显示内容
+    meta: {
+      title: '404页面-知乎日报',
+    },
+  },
+];
+
+export default routes;
